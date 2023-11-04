7a8a55bce33ffaf4cdc39be5b51d19ce2057f58d
yash <yashwanth.k@mooo.farm>
2023-11-04 23:13:55 +0530

    added input field

--- src/app/login/index.js
+++ src/app/login/index.js
@@ -3,11 +3,28 @@ import React, { useState } from "react";
 import styles from "./login.module.css";
 
 const LoginPage = () => {
+  const [userIdentifier, setUserIdentifier] = useState("");
+
+  const handleInputEmailId = (event) => {
+    setUserIdentifier(event.target.value);
+  };
+
   return (
     <>
       <div className={styles.main_container}>
         <div className={styles.login_container}>
           <div className={styles.login_header}>Login</div>
+          <div className={styles.login_section}>
+            <div className={styles.login_section_form}>
+              <input
+                type="text"
+                value={userIdentifier}
+                onChange={handleInputEmailId}
+                placeholder="Email/Mobile Number"
+                className={styles.login_sec_input}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
