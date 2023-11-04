a2fe1ed006434f2ba88589db08ec11adf1735f73
yash <yashwanth.k@mooo.farm>
2023-11-04 14:48:39 +0530

    initial setup

---
+++ src/app/login/index.js
@@ -0,0 +1,13 @@
+"use client";
+import React, { useState } from "react";
+import "./login.styles.css";
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="main_container">welcome to  login page</div>
+    </>
+  );
+};
+
+export default LoginPage;
