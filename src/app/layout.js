d86054a1db7b5a90c17d81dd834c61f9c25e782e
yash <yashwanth.k@mooo.farm>
2023-11-04 16:04:26 +0530

    minor changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "bwb",
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
