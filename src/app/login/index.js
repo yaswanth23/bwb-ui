21b7d6dc31996ef338e20baa8d48e768ba09a033
yash <yashwanth.k@mooo.farm>
2023-11-13 22:59:19 +0530

    integrated login api

--- src/app/login/index.js
+++ src/app/login/index.js
@@ -5,9 +5,12 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import Link from "next/link";
 
 const LoginPage = () => {
+  const apiUrl =
+    "https://qar5m2k5ra.execute-api.ap-south-1.amazonaws.com/dev/v2";
   const [userIdentifier, setUserIdentifier] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputUserIdentifier = (event) => {
     setUserIdentifier(event.target.value);
@@ -15,6 +18,46 @@ const LoginPage = () => {
 
   const handleInputPassword = (event) => {
     setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    const endpoint = apiUrl + "/auth/login";
+
+    if (!password) {
+      setErrorMessage("Please Enter Password");
+    }
+
+    if (!userIdentifier) {
+      setErrorMessage("Please Enter Email/Mobile Number");
+    }
+
+    if (userIdentifier && password) {
+      const reqData = {
+        userIdentifier: userIdentifier,
+        password: password,
+      };
+
+      fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data?.data?.statusCode === 200) {
+            setErrorMessage("");
+          }
+
+          if (data?.statusCode === 401) {
+            setErrorMessage(data?.message);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
   };
 
   return (
@@ -48,8 +91,13 @@ const LoginPage = () => {
                   {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
                 </div>
               </div>
+              <div className={styles.error_message_container}>
+                <span className={styles.error_message}>{errorMessage}</span>
+              </div>
               <div className={styles.button_container}>
-                <button className={styles.login_button}>Login</button>
+                <button className={styles.login_button} onClick={handleLogin}>
+                  Login
+                </button>
               </div>
               <div className={styles.contact_us_container}>
                 <p>
