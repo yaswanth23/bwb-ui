c82a604bdbafe9d67c475d35ac580c7463f88b06
yash <yashwanth.k@mooo.farm>
2023-11-20 20:28:11 +0530

    adding stepper func

--- src/app/dashboard/events/newEvent/newEvent.js
+++ src/app/dashboard/events/newEvent/newEvent.js
@@ -2,9 +2,38 @@
 
 import React, { useState } from "react";
 import styles from "./newEvent.module.css";
+import { Steps } from "antd";
 
 const NewEvent = () => {
-  return <div>new event page</div>;
+  const [stepCount, setStepCount] = useState(0);
+  const handleAddStepper = () => {
+    setStepCount(stepCount + 1);
+  };
+
+  return (
+    <>
+      <div className={styles.step_container}>
+        <Steps
+          progressDot
+          current={stepCount}
+          items={[
+            {
+              title: "Event Details",
+            },
+            {
+              title: "Product Details",
+            },
+            {
+              title: "Terms & Conditions",
+            },
+            {
+              title: "Publish",
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
 };
 
 export default NewEvent;
