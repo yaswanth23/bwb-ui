bc7a90a23385702dfff2395845924be32f5d3845
yash <yashwanth.k@mooo.farm>
2023-11-22 23:51:09 +0530

    added award event type

--- src/app/dashboard/events/newEvent/newEvent.js
+++ src/app/dashboard/events/newEvent/newEvent.js
@@ -3,9 +3,11 @@
 import React, { useState, useReducer } from "react";
 import styles from "./newEvent.module.css";
 import { Steps } from "antd";
+import Image from "next/image";
 import { TbEdit } from "react-icons/tb";
 import { FaRegCircleCheck, FaRegCircleXmark } from "react-icons/fa6";
 import { Tooltip } from "react-tooltip";
+import TaskImage from "../../../../../public/assets/images/tasks.png";
 
 const NewEvent = () => {
   const [stepCount, setStepCount] = useState(0);
@@ -98,6 +100,40 @@ const NewEvent = () => {
             </p>
             <Tooltip id="my-tooltip" className="custom-tooltip" />
           </div>
+        </div>
+        <div className={styles.award_section}>
+          <div className={styles.award_main}>
+            <div className={styles.award_radio_section}>
+              <h2>How will you award the event</h2>
+              <div className={styles.input_radio}>
+                <input
+                  type="radio"
+                  name="options"
+                  value="option1"
+                  checked={data.awardType === 1}
+                  onChange={() => updateData({ awardType: 1 })}
+                  id="1"
+                />
+                <label htmlFor="1">
+                  I'll award the entire lot to single vendor
+                </label>
+              </div>
+              <div className={styles.input_radio}>
+                <input
+                  type="radio"
+                  name="options"
+                  value="option1"
+                  checked={data.awardType === 2}
+                  onChange={() => updateData({ awardType: 2 })}
+                  id="2"
+                />
+                <label htmlFor="2">
+                  I may partially award the event to multiple vendors
+                </label>
+              </div>
+            </div>
+          </div>
+          <Image src={TaskImage} alt="task" className={styles.task_side_img} />
         </div>
       </div>
     </>
