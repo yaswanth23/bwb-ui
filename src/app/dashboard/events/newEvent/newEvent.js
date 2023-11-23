e08cf73669fa8da05e71f4717a06c6256496f412
yash <yashwanth.k@mooo.farm>
2023-11-23 08:48:57 +0530

    reduced unnecessary rendering and limited code

--- src/app/dashboard/events/newEvent/newEvent.js
+++ src/app/dashboard/events/newEvent/newEvent.js
@@ -4,14 +4,11 @@ import React, { useState, useReducer } from "react";
 import styles from "./newEvent.module.css";
 import { Steps } from "antd";
 import Image from "next/image";
-import { TbEdit } from "react-icons/tb";
-import { FaRegCircleCheck, FaRegCircleXmark } from "react-icons/fa6";
 import { Tooltip } from "react-tooltip";
 import TaskImage from "../../../../../public/assets/images/tasks.png";
 
 const NewEvent = () => {
   const [stepCount, setStepCount] = useState(0);
-  const [isEventTitleEditFlag, setIsEventTitleEditFlag] = useState(true);
   const EVENT_TYPE_DESCRIPTION = "Request for Quotation";
 
   const [data, updateData] = useReducer(
@@ -19,7 +16,7 @@ const NewEvent = () => {
       const updateData = { ...prev, ...next };
       return updateData;
     },
-    { eventTitle: "" }
+    { eventTitle: "", awardType: 1 }
   );
 
   return (
@@ -46,47 +43,14 @@ const NewEvent = () => {
       </div>
       <div className={styles.event_title_section}>
         <h1>Event Title:</h1>
-        {isEventTitleEditFlag ? (
-          <div className={styles.event_input_section_one}>
-            <input
-              type="text"
-              className={styles.event_type_input}
-              onChange={(e) => updateData({ eventTitle: e.target.value })}
-              placeholder="Enter Event Title"
-            />
-            <FaRegCircleCheck
-              className={styles.check_icon}
-              onClick={() => {
-                if (data.eventTitle) {
-                  setIsEventTitleEditFlag(false);
-                }
-              }}
-            />
-            <FaRegCircleXmark
-              className={styles.cancel_icon}
-              onClick={() => {
-                if (data.eventTitle) {
-                  setIsEventTitleEditFlag(false);
-                }
-              }}
-            />
-          </div>
-        ) : (
-          <div className={styles.event_input_section_two}>
-            <input
-              type="text"
-              defaultValue={data.eventTitle}
-              className={styles.event_type_input}
-              readOnly
-            />
-            <TbEdit
-              className={styles.edit_icon}
-              onClick={() => {
-                setIsEventTitleEditFlag(true);
-              }}
-            />
-          </div>
-        )}
+        <div className={styles.event_input_section}>
+          <input
+            type="text"
+            className={styles.event_type_input}
+            onChange={(e) => updateData({ eventTitle: e.target.value })}
+            placeholder="Enter Event Title"
+          />
+        </div>
       </div>
       <div className={styles.mid_container}>
         <div className={styles.event_details_section}>
