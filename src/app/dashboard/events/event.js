1e2c9eeddf9d3d41ae63f3350bcee08076260bea
yash <yashwanth.k@mooo.farm>
2023-11-30 10:09:03 +0530

    live upcoming events

--- src/app/dashboard/events/event.js
+++ src/app/dashboard/events/event.js
@@ -6,12 +6,22 @@ import Image from "next/image";
 import Event_bg from "../../../../public/assets/svg/new_event_bg.svg";
 import { FaPlus } from "react-icons/fa6";
 import NewEvent from "./newEvent/newEvent";
+import { useSelector } from "react-redux";
+import { selectUserData } from "@/store/user/user.selector";
+import { GiPlainCircle } from "react-icons/gi";
 
 const Event = () => {
+  const userData = useSelector(selectUserData);
   const [isNewEvent, setIsNewEvent] = useState(false);
+  const [eventData, setEventData] = useState([]);
+  const [status, setStatus] = useState("LIVE");
 
   const handleNewEvent = () => {
     setIsNewEvent(true);
+  };
+
+  const handleStatusChange = async (status) => {
+    setStatus(status);
   };
 
   return (
@@ -20,7 +30,7 @@ const Event = () => {
         <NewEvent />
       ) : (
         <>
-          <div className={styles.section}>
+          {/* <div className={styles.section}>
             <Image src={Event_bg} alt="event bg" className={styles.event_bg} />
             <p className={styles.no_event_txt}>No events Live right now.</p>
             <p className={styles.click_here_event_txt}>
@@ -28,6 +38,36 @@ const Event = () => {
             </p>
           </div>
           <div className={styles.new_event_btn_container}>
+            <button className={styles.new_event_btn} onClick={handleNewEvent}>
+              New Event <FaPlus />
+            </button>
+          </div> */}
+          
+          <div className={styles.event_section}>
+            <div className={styles.event_headers_section}>
+              <div
+                className={`${styles.live_btn} ${
+                  status === "LIVE" && styles.live_active
+                }`}
+                onClick={() => handleStatusChange("LIVE")}
+              >
+                Live
+                {status === "LIVE" && (
+                  <GiPlainCircle className={styles.circle_icon} />
+                )}
+              </div>
+              <div
+                className={`${styles.upcoming_btn} ${
+                  status === "UPCOMING" && styles.upcoming_active
+                }`}
+                onClick={() => handleStatusChange("UPCOMING")}
+              >
+                Upcoming
+                {status === "UPCOMING" && (
+                  <GiPlainCircle className={styles.circle_icon} />
+                )}
+              </div>
+            </div>
             <button className={styles.new_event_btn} onClick={handleNewEvent}>
               New Event <FaPlus />
             </button>
