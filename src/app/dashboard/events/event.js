c1cd11b992ba83aad486b9e7eaf28889e70674f5
yash <yashwanth.k@mooo.farm>
2023-11-30 10:18:52 +0530

    event page changes

--- src/app/dashboard/events/event.js
+++ src/app/dashboard/events/event.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./event.module.css";
 import Image from "next/image";
 import Event_bg from "../../../../public/assets/svg/new_event_bg.svg";
@@ -9,6 +9,7 @@ import NewEvent from "./newEvent/newEvent";
 import { useSelector } from "react-redux";
 import { selectUserData } from "@/store/user/user.selector";
 import { GiPlainCircle } from "react-icons/gi";
+import { getEvents } from "@/utils/api/event";
 
 const Event = () => {
   const userData = useSelector(selectUserData);
@@ -22,7 +23,22 @@ const Event = () => {
 
   const handleStatusChange = async (status) => {
     setStatus(status);
+    const data = await getEvents(userData.userId, status);
+    if (data?.data) {
+      setEventData(data.data.events);
+    }
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getEvents(userData.userId, status);
+      if (data?.data) {
+        setEventData(data.data.events);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -30,19 +46,6 @@ const Event = () => {
         <NewEvent />
       ) : (
         <>
-          {/* <div className={styles.section}>
-            <Image src={Event_bg} alt="event bg" className={styles.event_bg} />
-            <p className={styles.no_event_txt}>No events Live right now.</p>
-            <p className={styles.click_here_event_txt}>
-              Click here to create a new event
-            </p>
-          </div>
-          <div className={styles.new_event_btn_container}>
-            <button className={styles.new_event_btn} onClick={handleNewEvent}>
-              New Event <FaPlus />
-            </button>
-          </div> */}
-          
           <div className={styles.event_section}>
             <div className={styles.event_headers_section}>
               <div
@@ -71,6 +74,39 @@ const Event = () => {
             <button className={styles.new_event_btn} onClick={handleNewEvent}>
               New Event <FaPlus />
             </button>
+          </div>
+          <div>
+            {eventData.length > 0 ? (
+              <>
+                {eventData.map((event) => (
+                  <div key={event.eventid}>Event name: {event.eventname}</div>
+                ))}
+              </>
+            ) : (
+              <>
+                <div className={styles.section}>
+                  <Image
+                    src={Event_bg}
+                    alt="event bg"
+                    className={styles.event_bg}
+                  />
+                  <p className={styles.no_event_txt}>
+                    No events Live right now.
+                  </p>
+                  <p className={styles.click_here_event_txt}>
+                    Click here to create a new event
+                  </p>
+                </div>
+                <div className={styles.new_event_btn_container}>
+                  <button
+                    className={styles.new_event_btn}
+                    onClick={handleNewEvent}
+                  >
+                    New Event <FaPlus />
+                  </button>
+                </div>
+              </>
+            )}
           </div>
         </>
       )}
