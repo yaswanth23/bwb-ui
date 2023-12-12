7e70973b1e504fa7cb557282cbaf6125e1b32e8e
yash <yashwanth.k@mooo.farm>
2023-12-12 15:42:49 +0530

    table changes

--- src/app/dashboard/events/view/view.js
+++ src/app/dashboard/events/view/view.js
@@ -9,7 +9,7 @@ import { getUserEventDetails } from "@/utils/api/event";
 const View = ({ data }) => {
   const userData = useSelector(selectUserData);
   const [eventDetails, setEventDetails] = useState(null);
-
+  console.log(eventDetails);
   useEffect(() => {
     const fetchData = async () => {
       const response = await getUserEventDetails(userData.userId, data.eventid);
@@ -26,6 +26,38 @@ const View = ({ data }) => {
       <div className={styles.container}>
         <div className={styles.first_section}>
           <h1>{data.eventname}</h1>
+        </div>
+        <div className={styles.product_section}>
+          {eventDetails &&
+            eventDetails.productDetails.map((item) => (
+              <div key={item.productid} className={styles.product_card_section}>
+                <table className={styles.table_container}>
+                  <tbody>
+                    <tr>
+                      <th className={styles.product_headers}>
+                        <h2>{item.product}</h2>
+                        <p>Requested at {item.deliverylocation}</p>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th className={styles.product_sub_headers}>
+                        Quantity req: {item.quantity}
+                      </th>
+                    </tr>
+                    <tr>
+                      <th className={styles.product_sub_headers}>
+                        Price per unit
+                      </th>
+                    </tr>
+                    <tr>
+                      <th className={styles.product_sub_headers}>
+                        Total Amount
+                      </th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            ))}
         </div>
       </div>
     </>
