c7527ac61a3ead8e2f0e2a9544d7354be18f8f0f
yash <yashwanth.k@mooo.farm>
2023-12-12 13:10:42 +0530

    minor changes

--- src/app/dashboard/events/event.js
+++ src/app/dashboard/events/event.js
@@ -12,18 +12,24 @@ import { GiPlainCircle } from "react-icons/gi";
 import { getEvents } from "@/utils/api/event";
 import Countdown from "./countDown/countDown";
 import EndCountdown from "./endCountDown/endCountDown";
+import View from "./view/view";
 
 const Event = () => {
   const userData = useSelector(selectUserData);
   const [isNewEvent, setIsNewEvent] = useState(false);
   const [eventData, setEventData] = useState([]);
   const [status, setStatus] = useState("LIVE");
+  const [isView, setIsView] = useState(false);
+  const [viewEventData, setViewEventData] = useState({});
 
   const handleNewEvent = () => {
     setIsNewEvent(true);
   };
 
   const handleStatusChange = async (status) => {
+    if (isView) {
+      setIsView(false);
+    }
     setStatus(status);
     const data = await getEvents(userData.userId, status);
     if (data?.data) {
@@ -41,6 +47,11 @@ const Event = () => {
 
     fetchData();
   }, [status]);
+
+  const handleEventView = (event) => {
+    setViewEventData(event);
+    setIsView(true);
+  };
 
   return (
     <div className={styles.container}>
@@ -78,67 +89,79 @@ const Event = () => {
             </button>
           </div>
           <div>
-            {eventData.length > 0 ? (
+            {!isView ? (
               <>
-                <div className={styles.event_card_section}>
-                  {eventData.map((event) => (
-                    <div key={event.eventid} className={styles.event_card}>
-                      <h1 className={styles.event_heading}>
-                        {event.eventname}
-                      </h1>
-                      <div className={styles.event_rfq_section}>
-                        <h1>RFQ</h1>
-                        <p>
-                          {
-                            JSON.parse(event.eventAttributesStore[0].value)
-                              .length
-                          }
-                          {JSON.parse(event.eventAttributesStore[0].value)
-                            .length > 1
-                            ? " Products"
-                            : " Product"}
-                        </p>
-                      </div>
-                      {status === "LIVE" ? (
-                        <div className={styles.live_counter_section}>
-                          <EndCountdown
-                            eventStartTime={event.eventstarttime}
-                            eventDuration={event.eventduration}
-                          />
+                {eventData.length > 0 ? (
+                  <>
+                    <div className={styles.event_card_section}>
+                      {eventData.map((event) => (
+                        <div
+                          key={event.eventid}
+                          className={styles.event_card}
+                          onClick={() => handleEventView(event)}
+                        >
+                          <h1 className={styles.event_heading}>
+                            {event.eventname}
+                          </h1>
+                          <div className={styles.event_rfq_section}>
+                            <h1>RFQ</h1>
+                            <p>
+                              {
+                                JSON.parse(event.eventAttributesStore[0].value)
+                                  .length
+                              }
+                              {JSON.parse(event.eventAttributesStore[0].value)
+                                .length > 1
+                                ? " Products"
+                                : " Product"}
+                            </p>
+                          </div>
+                          {status === "LIVE" ? (
+                            <div className={styles.live_counter_section}>
+                              <EndCountdown
+                                eventStartTime={event.eventstarttime}
+                                eventDuration={event.eventduration}
+                              />
+                            </div>
+                          ) : (
+                            <div className={styles.upcoming_counter_section}>
+                              <Countdown
+                                eventStartTime={event.eventstarttime}
+                              />
+                            </div>
+                          )}
                         </div>
-                      ) : (
-                        <div className={styles.upcoming_counter_section}>
-                          <Countdown eventStartTime={event.eventstarttime} />
-                        </div>
-                      )}
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  </>
+                ) : (
+                  <>
+                    <div className={styles.section}>
+                      <Image
+                        src={Event_bg}
+                        alt="event bg"
+                        className={styles.event_bg}
+                      />
+                      <p className={styles.no_event_txt}>
+                        No events Live right now.
+                      </p>
+                      <p className={styles.click_here_event_txt}>
+                        Click here to create a new event
+                      </p>
+                    </div>
+                    <div className={styles.new_event_btn_container}>
+                      <button
+                        className={styles.new_event_btn}
+                        onClick={handleNewEvent}
+                      >
+                        New Event <FaPlus />
+                      </button>
+                    </div>
+                  </>
+                )}
               </>
             ) : (
-              <>
-                <div className={styles.section}>
-                  <Image
-                    src={Event_bg}
-                    alt="event bg"
-                    className={styles.event_bg}
-                  />
-                  <p className={styles.no_event_txt}>
-                    No events Live right now.
-                  </p>
-                  <p className={styles.click_here_event_txt}>
-                    Click here to create a new event
-                  </p>
-                </div>
-                <div className={styles.new_event_btn_container}>
-                  <button
-                    className={styles.new_event_btn}
-                    onClick={handleNewEvent}
-                  >
-                    New Event <FaPlus />
-                  </button>
-                </div>
-              </>
+              <View data={viewEventData} />
             )}
           </div>
         </>
---
+++ src/app/dashboard/events/view/view.js
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useEffect, useState, useReducer } from "react";
+import styles from "./view.module.css";
+import { useSelector } from "react-redux";
+
+const View = ({ data }) => {
+  return <div>hi</div>;
+};
+
+export default View;
